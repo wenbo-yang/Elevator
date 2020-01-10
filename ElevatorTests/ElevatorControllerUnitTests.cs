8aa1bb1552e01bc997783658c04dfbf45d3c8b75
Wenbo Yang <wenbo.yang.84@gmail.com>
2020-01-09 20:52:03 -0800

    change path

--- ElevatorTests/ElevatorControllerUnitTests.cs
+++ ElevatorTests/ElevatorControllerUnitTests.cs
@@ -3,19 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ElevatorTests
 {
-    // Idea: simulation
-    // In this simulation, I am assuming if an elevator goes to the next floor it takes 1 unit of time.
-    // If there is a stop situation it takes 2 units of time
-    // The elevator class contains a method Next(), this would run to next time increment. 
-    // If it reaches a stop, it will wait, and if it is going it will change position by 1 floor
-    // GetBestElevator() would run this simulation
-    // it will call Next() for all elevators
-    // during the process, it will cache the state for each elevator. 
-    // If at a particular time slot it picked up, then the state changes from Going to PickedUp
-    // and when we reached the destination from the Pickedup state, we will break out of the loop. 
 
-    // Big O: Time = O(T) where T = numberOfFloors * (elevator speed + elevator open and close door time) * number of elevators (K * F * N)
-    //        Space = O(N) where N = number of elevators
 
     [TestClass]
     public class ElevatorControllerUnitTests
